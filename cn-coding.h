b2b207a6799c1f7f0b6ca848c6495b9a33fea124
Cary0722 <167656032+bili-cary0722@users.noreply.github.com>
2024-05-04 14:56:56 +0800

    Add files via upload

---
+++ cn-coding.h
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+// 定义一些常用的变量类型
+#define 整数 int
+#define 浮点数 double
+#define 字符 char
+#define 布尔 bool
+#define 字符串 string
+
+// 定义一些常用的关键字
+#define 真 true
+#define 假 false
+#define 返回 return
+#define 结束 break
+#define 继续 continue
+#define main 主函数
+#define 赋值 =
+#define 等于 ==
+#define 加 +
+#define 减 -
+#define 乘 *
+#define 除 /
+#define 输入流 >>
+#define 输出流 <<
+#define 引用 using
+#define 标准命名空间 namespace std;
+
+// 定义一些常用的函数
+#define 输入 cin
+#define 输出 cout
+#define 输入 cin 
+#define 输出 cout
+#define 换行 endl
+#define 循环一 for
+#define 循环二 while
+
+//定义一些数字
+#define 零 0
+#define 一 1
+#define 二 2
+#define 三 3
+#define 四 4
+#define 五 5
+#define 六 6
+#define 七 7
+#define 八 8
+#define 九 9
+
+// 定义一些常用的符号
+#define ， ,
+#define ： :
+#define ； ;
+#define （ (
+#define ） )
+#define 开始 {
+#define 结束 }
+#define 左括号 (
+#define 右括号 )
+
+#define 空格 ' '
+#define 回车 '\n'
+#define 反斜杠 '\\'
+#define 单引号 '
+#define 双引号 "
+#define 问号 ?
